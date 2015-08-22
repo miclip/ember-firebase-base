f3737cd245b7d79505f523843cb10e936f46277c
miclip <mikel@mikes.com.au>
2015-08-21 22:27:19 -0400

    login pages

--- app/helpers/label.js
+++ app/helpers/label.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function label(params, hash) {
-  let parsedHtml = $('<label for="'+hash.for+'" class="'+hash.class+'" />').html(params[0])
+  let parsedHtml = $('<label for="'+hash.for+'" class="'+hash.class+'" />').html(params[0]);
   return new Ember.Handlebars.SafeString(parsedHtml[0].outerHTML);
 };
 
