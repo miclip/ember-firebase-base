2ade917923434512dc8554481ffb583d4965c6cb
miclip <mikel@mikes.com.au>
2015-08-28 06:11:15 -0400

    added sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,34 +1,22 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var funnel = require('ember-cli/node_modules/broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-  app.import(app.bowerDirectory + '/font-awesome/css/font-awesome.css');
-  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
   app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
   app.import(app.bowerDirectory + '/JavaScript-MD5/js/md5.js');
-  
-  app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff', {
+  var fontawesome = new funnel(app.bowerDirectory + '/font-awesome/fonts', {
+    srcDir: '/',
     destDir: 'fonts'
   });
-  app.import(app.bowerDirectory + '/font-awesome/fonts/fontawesome-webfont.woff2', {
-    destDir: 'fonts'
+  var merged = mergeTrees([app.toTree(), fontawesome], {
+     overwrite: true
   });
- 
-  return app.toTree();
+
+  return app.toTree(merged);f 
 };
